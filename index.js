c4ffbc0276a5001ea7162c59a19f1fb95826df52
aaltansa <arsalnaufal2007@gmail.com>
2025-02-18 10:43:58 +0700

    change origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = 3001;
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "http://localhost:5173"],
+  origin: ["*"],
   methods: ["GET", "POST", "PUT", "DELETE"],
 }
 
